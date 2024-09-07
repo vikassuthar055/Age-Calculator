0948f7fc70b32649ff161046d0de08ddcb31eeb4
vikas <sutharvikas3291@gmail.com>
2024-09-07 22:59:09 +0530

    first commit

---
+++ my.js
@@ -0,0 +1,43 @@
+let userInput = document.getElementById("date");
+userInput.max = new Date().toISOString().split("T")[0];
+let result = document.getElementById("result");
+
+function calculateage(){
+    let birthDate = new Date(userInput.value);
+    
+    let d1 = birthDate.getDate();
+    let m1 = birthDate.getMonth() + 1;
+    let y1 = birthDate.getFullYear();
+
+    let today = new Date();
+
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+    let d3, m3, y3;
+    y3=y2-y1;
+    
+    if(m2 >= m1){
+        m3 = m2-m1;
+    } else{
+        y3--;
+        m3 = 12 + m2 -m1;
+    }
+    if(d2 >=d1){
+        d3 = d2 - d1;
+    }else{
+        m3--;
+        d3 = getdaysInMonth(y1, m1) +d2 -d1;
+    }
+    if (m3<0){
+        m3 = 11;
+        y3--;
+    }
+     result.innerHTML = `You are ${y3} Year, ${m3} Month, ${d3} Day old.`;
+
+
+}
+function getdaysInMonth(year, month){
+    return new Date(year, month, 0).getDate;
+}
